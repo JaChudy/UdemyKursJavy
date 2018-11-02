7c085f13d2383085b6ee7234fa29b53c793f75b4
JaChudy <jakub.chudy@gmail.com>
2018-11-02 07:48:21 +0100

    statycznosc

--- src/com/company/programowanieobiektowe/statyczność/Statyczność.java
+++ src/com/company/programowanieobiektowe/statyczność/Statyczność.java
@@ -1,4 +1,43 @@
 package com.company.programowanieobiektowe.statyczność;
 
 public class Statyczność {
+
+    public static void main(String[] args) {
+//        Matematyka test = new Matematyka();
+//
+        double wynik = Matematyka.dodaj(10,20);
+
+        System.out.println(wynik);
+        System.out.println(Matematyka.PI);
+
+        Klient a = new Klient("Radek");
+        Klient b = new Klient("Arkadiusz");
+        Klient c = new Klient("Dupa");
+
+        System.out.println(a.id);
+        System.out.println(b.id);
+        System.out.println(c.id);
+    }
+}
+
+class Matematyka
+{
+    static final double PI = 3.14;
+    static double dodaj(double a, double b){
+        return a + b;
+    }
+}
+
+class Klient{
+
+    public Klient(String imie) {
+        this.imie = imie;
+        //this.id = id;
+        nastepnyID++;
+        id = nastepnyID;
+    }
+
+    String imie;
+    int id = 0;
+    static int nastepnyID = 0;
 }
