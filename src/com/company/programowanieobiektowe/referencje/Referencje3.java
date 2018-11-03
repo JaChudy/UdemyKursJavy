d20ec21d5f3092f2edcd531dce16f93a74e77a30
JaChudy <jakub.chudy@gmail.com>
2018-11-03 22:14:45 +0100

    referencje cd

--- src/com/company/programowanieobiektowe/referencje/Referencje3.java
+++ src/com/company/programowanieobiektowe/referencje/Referencje3.java
@@ -4,6 +4,12 @@ public class Referencje3 {
 
     public static void main(String[] args) {
 
+        String imie = new String("jakub");
+        String imie2 = new String("jakub"); // te same ciagi znakow trzymane sa w tym samamym miejscu w pamięci porownuje adresy
 
+
+        if(imie.equals(imie2)){ //Strinig porownujemy metodą equals
+            System.out.println("sa rowne");
+        }
     }
 }
