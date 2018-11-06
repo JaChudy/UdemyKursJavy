0c9e7c5fdcde8982fda8bbe35697aa9a722f0bb8
JaChudy <jakub.chudy@gmail.com>
2018-11-06 08:41:06 +0100

    exercise

--- src/com/company/programowanieobiektowe/cwiczenia/Butelka.java
+++ src/com/company/programowanieobiektowe/cwiczenia/Butelka.java
@@ -4,8 +4,45 @@ public class Butelka {
 
     double volume;
 
+    public Butelka() { //pusty domyslny konstruktor
+    }
+
     public Butelka(double volume) {
         this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    void  wlej(double ilosc){
+        this.volume += ilosc;
+    }
+
+    boolean wylej(double ilosc){
+        if(ilosc <= this.volume) {
+            this.volume -= ilosc;
+        }
+        else{
+            //System.out.println("Nie możesz wylać " + ilosc + " poniewaz w butelce jest tylko " + this.volume);
+            return false;
+        }
+        return true;
+    }
+
+    void przelej(double ilosc, Butelka gdziePrzelac){
+
+        if(this.wylej(ilosc))
+        {
+            gdziePrzelac.wlej(ilosc);
+        }
+        else{
+            System.out.println("nie da sie");
+        }
     }
 
     public static void main(String[] args) {
@@ -17,7 +54,18 @@ public class Butelka {
      //   }
 
         for(int i = 0; i < butelka.length; i++){
-            butelka[i] = new Butelka(12);
+            butelka[i] = new Butelka(2 + i );
+        }
+
+        butelka[1].wlej(5);
+        butelka[2].wylej(3);
+        butelka[3].wylej(10);
+
+        butelka[4].przelej(2, butelka[5]);
+
+
+        for(Butelka but: butelka){
+            System.out.println(but.getVolume());
         }
 
 
